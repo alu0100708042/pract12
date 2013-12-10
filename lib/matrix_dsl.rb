13e85ce78890b42ffc800b411ec65220f2cd9490
Ricardo Oliva <alu0100708042@ull.edu.es>
2013-12-10 20:47:26 +0000

    Añadiendo initialize de la clase MatrixDSL

--- lib/matrix_dsl.rb
+++ lib/matrix_dsl.rb
@@ -4,6 +4,28 @@ require 'matrix.rb'
 # Fecha: 		10/12/2013
 # Descripción: 	Implementación de la clase MatrixDSL, para implementar un DSL sobre matrices.
 class MatrixDSL < Matrix
+   # Al initialize se le pasa el tipo de operación que se desea realizar
+        def initialize(operation)
+
+                @operacion = operacion.to_s
+                
+             # El case asignara el operando
+                case @operandoeracion       
+
+             	   when "suma"
+                        @operando= "+"                
+               	   when "resta"
+                        @operando = "-"
+                   when "multiplica"
+                        @operando = "*"             
+                end
+             # se inicializan dos matrices que son las que realizarán la operación a nil                           
+                @matrixA = nil
+                @matrixB = nil
+
+        end
+
+        attr_reader :matrizA, :matrizB
 
 end
 
