705b3a2d82b6efc8de4162b7fb2edf5b35cfe902
Ricardo Oliva <alu0100708042@ull.edu.es>
2013-12-10 20:45:36 +0000

    añadiendo fichero matrix_dsl.rb para implementar el DSL

---
+++ lib/matrix_dsl.rb
@@ -0,0 +1,10 @@
+require 'matrix.rb'
+#encoding UTF-8
+# Autor: 		Ricardo Oliva García, J. Oliver Martínez Novo
+# Fecha: 		10/12/2013
+# Descripción: 	Implementación de la clase MatrixDSL, para implementar un DSL sobre matrices.
+class MatrixDSL < Matrix
+
+end
+
+
