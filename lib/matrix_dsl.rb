bf06144257c30197020b78a7c3e19d0a220682d0
Oliver Martínez Novo <alu0100704129@ull.edu.es>
2013-12-11 00:27:14 +0000

    Declarando función execute para el DSL

--- lib/matrix_dsl.rb
+++ lib/matrix_dsl.rb
@@ -3,45 +3,65 @@ require 'matrix.rb'
 # Autor: 		Ricardo Oliva García, J. Oliver Martínez Novo
 # Fecha: 		10/12/2013
 # Descripción: 	Implementación de la clase MatrixDSL, para implementar un DSL sobre matrices.
+
 class MatrixDSL < Matrix
-   # Al initialize se le pasa el tipo de operación que se desea realizar
-        def initialize(operation)
+	
+	# Al initialize se le pasa el tipo de operación que se desea realizar
+	def initialize(operation)
 
-                @operacion = operacion.to_s
-                
-             # El case asignara el operando
-                case @operandoeracion       
+		@operacion = operacion.to_s
+			
+		# El case asignara el operando
+		case @operandoeracion       
 
-             	   when "suma"
-                        @operando= "+"                
-               	   when "resta"
-                        @operando = "-"
-                   when "multiplica"
-                        @operando = "*"             
-                end
-             # se inicializan dos matrices que son las que realizarán la operación a nil                           
-                @matrixA = nil
-                @matrixB = nil
+			when "suma"
+				@operando= "+"                
+			when "resta"
+				@operando = "-"
+			when "multiplica"
+				@operando = "*"             
+		end
+		
+		# se inicializan dos matrices que son las que realizarán la operación a nil                           
+		@matrixA = nil
+		@matrixB = nil
 
-        end
+	end
 
-	       # en option se dirá si son dispersas o densas
-        def option(opcion)
-                @tipoMatrix = opcion                
-        end
+	# en option se dirá si son dispersas o densas
+	def option(opcion)
+		@tipoMatrix = opcion                
+	end
 
-        # Se define operand que contendra cara uno de los operandos de la matriz
-        def operand(other)
-                
-                if(@matrixA == nil)
-                        @matrixA = other
-                else
-                        @matrixB = other
-                end
-                             
-                execute
-                
-        end
+	# Se define operand que contendra cara uno de los operandos de la matriz
+	def operand(other)
+		if(@matrixA == nil)
+			@matrixA = other
+		else
+			@matrixB = other
+		end			 
+		execute
+	end
+		
+	# Definimos la función de ejecución del DSL
+	def execute
+			
+		# Mostramos las matrices
+		if @matrixA != nil and @matrixB != nil
+
+			@matrizA = "Matrix" + @tipoMatrix.to_s + ".new(@matrixA)"
+			puts "Matriz A: #{@matrixA}"
+
+			@matrizB = "Matrix" + @tipoMatrix.to_s + ".new(@matrixB)"
+			puts "Matriz B: #{@matrixB}"
+
+			resultado = @matrizA.to_s + "." + @operando.to_s + "(" + " " + @matrizB.to_s + ")"
+			resultado = eval(resultado)
+			puts "Resultado de la operacion ( " + @operacion.to_s + " ): #{resultado}"
+
+		end
+			
+	end
 
 end
 
